674c5ab844bf80c7aefd23a2ee226466193c4398
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-03-27 19:40:14 -0300

    feat: creates fonts theme

---
+++ web/src/styles/themes/fonts.ts
@@ -0,0 +1,20 @@
+export const fonts = {
+  family: {
+    primary: "'Poppins', sans-serif",
+  },
+  size: {
+    xs: "0.75rem",
+    sm: "0.875rem",
+    md: "1rem",
+    lg: "1.125rem",
+    xl: "1.25rem",
+    xl1: "1.5rem",
+    xl2: "2rem",
+  },
+  weight: {
+    normal: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+  },
+};

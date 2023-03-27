8ca7f160eaf3bd29a4f6f777550675455069c246
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-03-27 19:40:05 -0300

    feat: creates theme styles

---
+++ web/src/styles/themes/border-radius.ts
@@ -0,0 +1,7 @@
+export const borderRadius = {
+  none: "0",
+  sm: "0.625rem",
+  md: "1rem",
+  lg: "1.875rem",
+  full: "9999px",
+};
---
+++ web/src/styles/themes/index.ts
@@ -0,0 +1,9 @@
+import { borderRadius } from "./border-radius";
+import { colors } from "./colors";
+import { fonts } from "./fonts";
+
+export const theme = {
+  colors,
+  borderRadius,
+  fonts,
+};
